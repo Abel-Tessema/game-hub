3f60b885443325520a453cdb83dd284b5beabf6e
Bela Jash <abelxotessema@gmail.com>
2024-06-27 13:35:16 +0300

    Display the genres.

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,24 @@ function App() {
     return (
         <Grid
             templateAreas={{
-            base: `"nav" "main"`,
-            lg: `"nav nav" "aside main"`
-        }}
+                base: `"nav" "main"`,
+                lg: `"nav nav" "aside main"`
+            }}
+            templateColumns={{
+                base: "1fr",
+                lg: "200px 1fr"
+            }}
         >
             <GridItem area="nav">
-                <NavBar />
+                <NavBar/>
             </GridItem>
             <Show above="lg">
-                <GridItem area="aside">
-                    <GenreList />
+                <GridItem area="aside" paddingX="20px">
+                    <GenreList/>
                 </GridItem>
             </Show>
             <GridItem area="main">
-                <GameGrid />
+                <GameGrid/>
             </GridItem>
 
         </Grid>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,29 @@
 import useGenres from "../hooks/useGenres.ts";
+import {HStack, Image, List, ListItem, Text} from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url.ts";
 
 function GenreList() {
     const {data} = useGenres();
     return (
-        <ul>
-            {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
-        </ul>
+        <List>
+            {data.map(genre =>
+                <ListItem key={genre.id} paddingY="5px">
+                    <HStack>
+                        <Image
+                            src={getCroppedImageUrl(genre.image_background)}
+                            boxSize="32px"
+                            borderRadius="8px"
+                        />
+                        <Text
+                            key={genre.id}
+                            fontSize="lg"
+                        >
+                            {genre.name}
+                        </Text>
+                    </HStack>
+                </ListItem>
+            )}
+        </List>
     );
 }
 
