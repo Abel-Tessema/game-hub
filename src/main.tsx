dc99d96f3ffb3c34f937d9a281320efce714960f
Bela Jash <abelxotessema@gmail.com>
2024-06-28 15:32:32 +0300

    Add Vercel Analytics.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import {ChakraProvider, ColorModeScript} from '@chakra-ui/react'
 import App from './App.tsx'
 import theme from "./theme.ts";
 import './index.css'
+import {Analytics} from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -11,5 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <App />
       </ChakraProvider>
+      <Analytics />
   </React.StrictMode>,
 )
