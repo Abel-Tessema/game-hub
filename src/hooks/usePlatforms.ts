58dccd224bdf855e6cd208b6f08b2fd7eb5a23c0
Bela Jash <abelxotessema@gmail.com>
2024-07-03 10:48:00 +0300

    Refactor the platforms hook with React Query.

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,16 @@
-import useData from "./useData.ts";
+import {FetchResponse} from "./useData.ts";
 import {Platform} from "./useGames.ts";
+import {useQuery} from "@tanstack/react-query";
+import apiClient from "../services/api-client.ts";
+import platforms from "../data/platforms.ts";
 
-const usePlatforms = () =>
-    useData<Platform>("/platforms/lists/parents");
+const usePlatforms = () => useQuery<Platform[], Error>({
+    queryKey: ['platforms'],
+    queryFn: () => apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then(response => response.data.results),
+    staleTime: 24 * 60 * 60 * 1_000,
+    initialData: platforms,
+});
 
 export default usePlatforms;
