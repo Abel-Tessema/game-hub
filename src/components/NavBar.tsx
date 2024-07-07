9b128c0f212f03f1a9b8df96cb8ed7045e17b875
Bela Jash <abelxotessema@gmail.com>
2024-07-07 22:56:49 +0300

    Fix the navigation bar.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,12 @@ import {HStack, Image} from "@chakra-ui/react";
 import logo from "../assets/logo.webp"
 import ColorModeSwitch from "./ColorModeSwitch.tsx";
 import SearchInput from "./SearchInput.tsx";
+import {Link} from "react-router-dom";
 
 function NavBar() {
     return (
         <HStack paddingX={15} paddingY={3} paddingRight={7}>
-            <Image src={logo} alt="Game Hub" boxSize="60px"></Image>
+            <Link to="/"><Image src={logo} alt="Game Hub" boxSize="60px" objectFit="cover"/></Link>
             <SearchInput/>
             <ColorModeSwitch />
         </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,17 +2,23 @@ import {Box, Input, InputGroup, InputLeftElement} from "@chakra-ui/react";
 import {BsSearch} from "react-icons/bs";
 import {useRef} from "react";
 import useGameQueryStore from "../gameQueryStore.ts";
+import {useNavigate} from "react-router-dom";
 
 function SearchInput() {
     const ref = useRef<HTMLInputElement>(null);
 
     const setSearchText = useGameQueryStore(selector => selector.setSearchText);
 
+    const navigate = useNavigate();
+
     return (
         <Box marginX={5} width="100%">
             <form onSubmit={event => {
                 event.preventDefault();
-                if (ref.current) setSearchText(ref.current.value);
+                if (ref.current) {
+                    setSearchText(ref.current.value);
+                    navigate("/");
+                }
             }}>
                 <InputGroup>
                     <InputLeftElement children={<BsSearch/>}/>
